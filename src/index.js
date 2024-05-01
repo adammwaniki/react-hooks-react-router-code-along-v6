9be310d362b0d2b9a248c26f18c43688968811a7
Adam Mwaniki <adamndegwa@protonmail.com>
2024-05-02 00:51:48 +0300

    Makes notes on how to add more pages/components to route to

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 // Step 1. Import react-router functions
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+// Importing the other pages that we will be creating routes to
+import About from "./pages/About";
+import Login from "./pages/Login";
 
     // Step 2. Create a route object
     // createBrowserRouter is used to create the router for our application. 
@@ -13,7 +16,17 @@ const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />
+    }, 
+    // Adding new routes to the array of route objects
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/login",
+      element: <Login />
     }
+
   ]);
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
