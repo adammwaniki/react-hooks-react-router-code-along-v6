cd772035aa73cfd6ca7f0ed5f7e9e41536e36662
Adam Mwaniki <adamndegwa@protonmail.com>
2024-05-02 02:20:42 +0300

    Makes notes on useParams

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,6 +1,12 @@
 import NavBar from "../components/NavBar";
+// Now for the last piece of the puzzle - the useParams hook.
+import { useParams } from "react-router-dom";
 
 function UserProfile() {
+    // useParams allows us to access the data we've stored in our URL parameters and use it within our components.
+    // Note that the key is the parameter we defined in our route, and the value is what appears in the URL.
+    const params = useParams();
+    console.log(params);
   return(
     <>
       <header>
